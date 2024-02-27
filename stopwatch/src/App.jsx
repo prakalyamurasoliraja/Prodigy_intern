348c49de9239ac445de11e50fa220d9fbcde1e60
prakalyamurasoliraja <125952882+prakalyamurasoliraja@users.noreply.github.com>
2024-02-27 09:34:42 +0530

    Add files via upload

---
+++ stopwatch/src/App.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Particles from "react-particles-js";
+import Timer from "./Components/Timer";
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="timer">
+        <Particles
+          width="100vw"
+          height="100vh"
+          params={{
+            particles: {
+              number: {
+                value: 150
+              },
+              line_linked: {
+                shadow: {
+                  enable: true,
+                  color: "#3CA9D1",
+                  blur: 50
+                },
+                opacity: {
+                  value: 0.05
+                }
+              },
+              move: {
+                speed: 0.4
+              }
+            },
+            interactivity: {
+              detect_on: "canvas",
+              events: {
+                onhover: {
+                  enable: true,
+                  mode: "grab"
+                },
+                onclick: {
+                  enable: true,
+                  mode: "push"
+                }
+              }
+            }
+          }}
+        />
+        <Timer />
+        </div>
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
